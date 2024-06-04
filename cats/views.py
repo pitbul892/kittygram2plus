5ff51d91024c2c4465c5aecb1938b58d72b3cf0f
Pitbul892 <giredo92@yandex.ru>
2024-06-04 23:25:22 +0300

    3/5

--- cats/views.py
+++ cats/views.py
@@ -1,17 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 
 from .models import Achievement, Cat, User
-
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-
+    permission_classes = (OwnerOrReadOnly,) 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
 
+    def get_permissions(self):
+    # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions() 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
@@ -21,3 +28,5 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 class AchievementViewSet(viewsets.ModelViewSet):
     queryset = Achievement.objects.all()
     serializer_class = AchievementSerializer
+
+    
---
+++ requests/requests.py
@@ -0,0 +1,34 @@
+{
+    "first_name": "Roma",
+    "last_name": "Kopytov",
+}
+
+{
+    "name": "Sisya",
+    "color": "black",
+    "birth_year": 2021,
+    "owner": 1
+}
+
+{
+    "name": "Барсик",
+    "color": "White",
+    "birth_year": 2017,
+    "achievements": [
+        {"achievement_name": "поймал мышку"},
+        {"achievement_name": "разбил вазу"}
+    ]
+}
+
+{
+    "username": "Author",
+    "password": "9293oderiG)"
+}
+eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3NjExNjY0LCJqdGkiOiIzOGVhMzBiZjk1ZGE0NDE1OTExYzliYTMyOTU2ZDdlOSIsInVzZXJfaWQiOjF9.XQ5POn7MCFrECEYdcQZp49HmvQgZfOWp0c2Ow-jqhBc
+
+{
+    "username": "Reader",
+    "password": "9293oderiG)"
+}
+
+eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3NjExNjM0LCJqdGkiOiI0YzA4ZWVjMzYwMjU0MGUxOGUwYWRmMzkyOTQ0ZmI2NCIsInVzZXJfaWQiOjJ9.u_UD6tZABBixTdlis74jnJc-tNvkPgcTwVyfBwJ2HSg
